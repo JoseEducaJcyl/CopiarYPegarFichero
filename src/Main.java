b1735251373c7801e8d4e0a17c7fbe72f0921c30
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-28 13:14:39 +0100

    Ejercicio sobre leer el texto de un archivo y escribirlo en otro

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.*;
+import java.util.ArrayList;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> textos = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new
+                FileReader("datos.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                textos.add(linea);
+                try (BufferedWriter writer = new BufferedWriter(new
+                        FileWriter("copia.txt"))) {
+                    for (String texto : textos) {
+                        for (int i = 0; i < texto.length(); i++) {
+                            char caracter = texto.charAt(i);
+                            if (caracter == '.') {
+                                writer.newLine();
+                            }
+                        }
+                        writer.write(texto);
+                    }
+                    System.out.println("Líneas escritas en el archivo.");
+                } catch (IOException e) {
+                    System.out.println("Ocurrió un error al escribir en el archivo: " +
+                            e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " +
+                    e.getMessage());
+        }
+
+
+
+    }
 }
