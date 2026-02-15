f207a7f49a45ff9413b70634150c750681cced51
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 16:24:24 +0100

    Comentarios agregados

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        //Declaramos una lista para los textos
         ArrayList<String> textos = new ArrayList<>();
+        //Leemos un fichero, en este caso "datos.txt" y guardamos los textos 
+        //en la lista 
         try (BufferedReader reader = new BufferedReader(new
                 FileReader("datos.txt"))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
                 textos.add(linea);
+                //Y creamos un fichero, en este caso "copia.txt" y a traves del
+                //contenido de la lista de textos, se rellena el fichero 
                 try (BufferedWriter writer = new BufferedWriter(new
                         FileWriter("copia.txt"))) {
                     for (String texto : textos) {
